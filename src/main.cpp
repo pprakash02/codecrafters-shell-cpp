d496a67963f198cede1addccc29656bd1c2da530
pprakash02 <11pprakash@gmail.com>
2026-01-03 03:59:28 +0530

    invalid command handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <string>
-
+using namespace std;
 int main() {
   // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  // TODO: Uncomment the code below to pass the first stage
-   std::cout << "$ ";
+  cout << unitbuf;
+  cerr << unitbuf;
+ 
+  std::cout << "$ ";
+   string command;
+  getline(cin,command);
+  if(command!=""){
+	  cout<<command<<": command not found";
+  }	
 }
