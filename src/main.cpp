8ec3c2858152ed5d17583e325395a1650252ebc0
pprakash02 <11pprakash@gmail.com>
2026-01-03 04:09:07 +0530

    repl

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main() {
-  // Flush after every std::cout / std:cerr
-  cout << unitbuf;
-  cerr << unitbuf;
- 
-  std::cout << "$ ";
-   string command;
-  getline(cin,command);
-  if(command!=""){
-	  cout<<command<<": command not found";
-  }	
+	// Flush after every std::cout / std:cerr
+	cout << unitbuf;
+	cerr << unitbuf;
+	
+	//REPL implementation
+	while(true){
+		//prompt
+		cout << "$ ";
+		
+		//command input
+		string command;
+		getline(cin,command);
+		
+		//invalid command handling
+		if(command!=""){
+			cout<<command<<": command not found";
+		}
+		cout<<"\n";
+	}	
 }
