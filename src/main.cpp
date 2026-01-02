2ac71e3183db1d00ee148c0db12aeed4615b5d77
pprakash02 <11pprakash@gmail.com>
2026-01-03 04:32:42 +0530

    type

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
+#include <set>
 using namespace std;
 
 int main() {
 	// Flush after every std::cout / std:cerr
 	cout << unitbuf;
 	cerr << unitbuf;
+	set<string> builtin_commands={"exit","echo","type"};
 	
 	//REPL implementation
 	while(true){
@@ -22,6 +24,14 @@ int main() {
 		}
 		else if(command.substr(0,4) == "echo"){
 			cout<<command.substr(5);
+		}
+		else if(command.substr(0,4) == "type"){
+			if(builtin_commands.count(command.substr(5))){
+				cout<<command.substr(5)<<" is a shell builtin";
+			}
+			else{
+				cout<<command.substr(5)<<": not found";
+			}
 		}
 		else{
 			cout<<command<<": command not found";
