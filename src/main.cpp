5d9de87473ce34577a6899ec70b6fb40cf667859
pprakash02 <11pprakash@gmail.com>
2026-01-04 04:51:53 +0530

    locate executable files

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,56 @@
 #include <iostream>
 #include <string>
 #include <set>
+#include <filesystem>
+#include <vector>
+#include <cstdlib>
 using namespace std;
+namespace fs = filesystem;
+
+
+vector<string> split_path(string path, const char delimiter){
+	vector<string> v;
+	string s="";
+	for(auto it = path.begin(); it!=path.end(); it++){
+		if(*it == delimiter){
+			v.push_back(s);
+			s="";
+		}
+		else{
+			s+=(*it);
+		}
+	}
+	return v;
+}
+
+bool fs_exists_and_exec(fs::path s){
+	fs::perms prms = fs::status(s).permissions();
+	constexpr fs::perms owner_execution = fs::perms::owner_exec;
+	return ((prms & owner_execution) != fs::perms::none);
+}
+
+
 
 int main() {
 	// Flush after every std::cout / std:cerr
 	cout << unitbuf;
 	cerr << unitbuf;
-	set<string> builtin_commands={"exit","echo","type"};
+	set<string> builtin_commands = {"exit","echo","type"};
 	
+	//environment preprocessor for os path split
+	#ifdef _WIN32
+		const char os_pathstep = ';';
+	#else
+		const char os_pathstep = ':';
+	#endif
+	
+	// getting path from env
+	const char* path_variable = getenv("PATH");
+	string path = "";
+	if(path_variable != nullptr){
+		path = path_variable;
+	}
+	vector<string> path_without_delimiter = split_path(path,os_pathstep);
 	//REPL implementation
 	while(true){
 		//prompt
@@ -26,11 +68,23 @@ int main() {
 			cout<<command.substr(5);
 		}
 		else if(command.substr(0,4) == "type"){
-			if(builtin_commands.count(command.substr(5))){
-				cout<<command.substr(5)<<" is a shell builtin";
+			string argument = command.substr(5);
+			bool invalid = true;
+			if(builtin_commands.count(argument) == 0){
+				for(auto x:path_without_delimiter){
+					if(fs_exists_and_exec(x+"/"+argument)){
+						cout<<argument<<" is "<<x+"/"+argument;
+						invalid = false;
+						break;
+					}
+				}
 			}
 			else{
-				cout<<command.substr(5)<<": not found";
+				cout<<argument<<" is a shell builtin";
+				invalid = false;
+			}
+			if(invalid){
+				cout<<argument<<": not found";
 			}
 		}
 		else{
