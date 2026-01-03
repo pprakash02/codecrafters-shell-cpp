f8f45e917bbd57539ee68def26d61d27c5c84d92
pprakash02 <11pprakash@gmail.com>
2026-01-04 05:00:50 +0530

    locate executable

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,8 @@ vector<string> split_path(string path, const char delimiter){
 
 bool fs_exists_and_exec(fs::path s){
 	error_code ec;
-	fs::perms prms = fs::status(s,ec).permissions();
-	if(ec) return false;
+	fs::perms prms = fs::status(s/*,ec*/).permissions();
+	//if(ec) return false;
 	constexpr fs::perms owner_execution = fs::perms::owner_exec;
 	return ((prms & owner_execution) != fs::perms::none);
 }
