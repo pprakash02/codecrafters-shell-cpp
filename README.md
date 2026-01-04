59a34dea6c49a6b2b78e915b07b1cb98e37572dc
pprakash02 <11pprakash@gmail.com>
2026-01-04 21:23:45 +0530

    run a program

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 [![progress-banner](https://backend.codecrafters.io/progress/shell/22d2cde2-a3f9-4fe8-8ef7-16324b8ea0d1)](https://app.codecrafters.io/users/codecrafters-bot?r=2qF)
 
 This is a starting point for C++ solutions to the
-["Build Your Own Shell" Challenge](https://app.codecrafters.io/courses/shell/overview).
+["Build Your Own Shell" Challenge].
 
 In this challenge, you'll build your own POSIX compliant shell that's capable of
 interpreting shell commands, running external programs and builtin commands like
 cd, pwd, echo and more. Along the way, you'll learn about shell command parsing,
 REPLs, builtin commands, and more.
 
-**Note**: If you're viewing this repo on GitHub, head over to
-[codecrafters.io](https://codecrafters.io) to try the challenge.
 
 # Passing the first stage
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,16 @@
 #include <filesystem>
 #include <vector>
 #include <cstdlib>
+#include <unistd.h>
 using namespace std;
 namespace fs = filesystem;
 
 
-vector<string> split_path(string path, const char delimiter){
+vector<string> split_string(string line, const char delimiter){
+	// function that splits a string into vector of strings based on delimiter
 	vector<string> v;
 	string s="";
-	for(auto it = path.begin(); it!=path.end(); it++){
+	for(auto it = line.begin(); it!=line.end(); it++){
 		if(*it == delimiter){
 			v.push_back(s);
 			s="";
@@ -24,6 +26,7 @@ vector<string> split_path(string path, const char delimiter){
 }
 
 bool fs_exists_and_exec(fs::path s){
+	// function that checks if file is readable and executable
 	error_code ec;
 	fs::perms prms = fs::status(s,ec).permissions();
 	if(ec) return false;
@@ -52,25 +55,43 @@ int main() {
 	if(path_variable != nullptr){
 		path = path_variable;
 	}
-	vector<string> path_without_delimiter = split_path(path,os_pathstep);
+	vector<string> path_without_delimiter = split_string(path,os_pathstep);
+	
 	//REPL implementation
 	while(true){
 		//prompt
 		cout << "$ ";
 		
 		//command input
-		string command;
-		getline(cin,command);
+		string line;
+		getline(cin,line);
+		line+=' '; //added so that split_string works uniformly
+		vector<string> command = split_string(line,' ');
 		
 		//invalid command handling
-		if(command == "exit"){
+		if(command[0] == "exit"){
 			break;
 		}
-		else if(command.substr(0,4) == "echo"){
-			cout<<command.substr(5);
+		else if(command[0] == "echo"){
+			auto it = command.begin();
+			it++;
+			if(it != command.end()){
+				cout<<*it;
+				it++;
+			}
+			while(it != command.end()){
+				cout<<" "<<*it;
+				it++;
+			}
 		}
-		else if(command.substr(0,4) == "type"){
-			string argument = command.substr(5);
+		else if(command[0] == "type"){
+			string argument = "";
+			if(command.size() > 1){
+				argument = command[1];
+			}
+			if(argument == ""){
+				continue;
+			}
 			bool invalid = true;
 			if(builtin_commands.count(argument) == 0){
 				for(auto x:path_without_delimiter){
@@ -84,13 +105,29 @@ int main() {
 			else{
 				cout<<argument<<" is a shell builtin";
 				invalid = false;
+				
 			}
 			if(invalid){
 				cout<<argument<<": not found";
 			}
 		}
 		else{
-			cout<<command<<": command not found";
+			bool notfound = true;
+			auto it = command.begin();
+			it++;
+			vector<char*> argv;
+			for(; it != command.end(); it++){
+				argv.push_back(const_cast<char*>((*it).c_str()));
+			}
+			argv.push_back(nullptr);
+			for(auto x:path_without_delimiter){
+				string path_string = x + "/" + command[0];
+				if(fs_exists_and_exec(path_string)){
+					execv(path_string.c_str(), argv.data());
+					break;
+				}
+			}
+			if(notfound)cout<<command[0]<<": command not found";
 		}
 		cout<<"\n";
 	}	
