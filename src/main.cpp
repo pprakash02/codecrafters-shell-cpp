01833c973e53e11062dc71d61c90018be74abc16
pprakash02 <11pprakash@gmail.com>
2026-01-05 23:41:52 +0530

    backslash within double quotes

--- src/main.cpp
+++ src/main.cpp
@@ -17,19 +17,19 @@ vector<string> split_string(string line, const char delimiter){
 	string s="";
 	long long ct1=0, ct2=0;
 	for(auto it = line.begin(); it!=line.end(); it++){
-		if(*it == '\\' && !(ct2&1)){
+		if(*it == '\\' && !(ct2&1)){	
 			it++;
 			s+=(*it);
 			continue;
+	
 		}
-		if(*it == '\\' && !(ct1&1)){
+		if(*it == '\\' && (ct1&1)){
 			it++;
-			if((*it) == '"' || (*it) == '\\' || (*it) == '$' || (*it) == '`' || (*it)=='n'){
-				if(*it != 'n')s+=(*it);
-				else s+=('\n');
+			if(*it == '"' || *it == '\\' || *it == '$' || *it == '`' || *it == '\n'){
+				s+=(*it);
 			}
 			else{
-				s+='\\';
+				it--;
 				s+=(*it);
 			}
 			continue;
